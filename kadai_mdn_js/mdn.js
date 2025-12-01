cfda89e7f7f53347b295ce148a0a41793db0fd6b
aumr0701 <aumr0701@gmail.com>
2025-12-01 12:08:09 +0900

    課題提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const theDay = new Date("2024-11-12T23:15:30");
+const day = theDay.getDate();
+const month = theDay.getMonth();
+const year = theDay.getFullYear();
+
+console.log(year + '年'+ month + '月' + day + '日');
+
+
