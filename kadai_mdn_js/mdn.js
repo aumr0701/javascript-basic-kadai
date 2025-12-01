69040d8748411c1d30cd67ebcba016c16dac33d2
aumr0701 <aumr0701@gmail.com>
2025-12-01 12:42:54 +0900

    3回目修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const today = new Date(2024, 10, 12);
 
 const day = today.getDate();
-const month = today.getMonth();
+const month = today.getMonth() + 1;
 const year = today.getFullYear();
 
 console.log(year + '年'+ month + '月' + day + '日');
